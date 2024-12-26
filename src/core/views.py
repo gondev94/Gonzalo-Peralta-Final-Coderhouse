a07527d9abce1373b5af0c2d866aca26f2ab68d3
Gonzalo Peralta <gonnzaperalta94@gmail.com>
2024-12-26 00:55:20 -0300

    migraciones creadas

--- src/core/views.py
+++ src/core/views.py
@@ -1,14 +1,58 @@
+from django.contrib import messages
+from django.contrib.auth.decorators import login_not_required  # type: ignore
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib.auth.views import LoginView
+from django.forms import BaseModelForm
+from django.http import HttpResponse
 from django.shortcuts import render
-from django.http import HttpResponse, HttpRequest
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, UpdateView
+
+from .forms import CustomAuthenticationForm, CustomUserCreationForm, UserProfileForm
 
 
-
-
+@login_not_required
 def index(request):
-    return render(request, "core/index.html", name="index")
+    return render(request, 'core/index.html')
 
+
+@login_not_required
 def about(request):
-    return render(request, "core/about.html", name="about")
+    return render(request, 'core/about.html')
 
-def base(request):
-    return render(request, "core/base.html", name="base")
+
+class CustomLoginView(LoginView):
+    authentication_form = CustomAuthenticationForm
+    template_name = 'core/login.html'
+    next_page = reverse_lazy('core:index')
+
+    def form_valid(self, form: AuthenticationForm) -> HttpResponse:
+        usuario = form.get_user()
+        messages.success(
+            self.request, f'Inicio de sesión exitoso ¡Bienvenido {usuario.username}!'
+        )
+        return super().form_valid(form)
+
+
+@method_decorator(login_not_required, name='dispatch')
+class CustomRegisterView(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'core/register.html'
+    success_url = reverse_lazy('core:login')
+
+    def form_valid(self, form: BaseModelForm) -> HttpResponse:
+        messages.success(self.request, 'Registro exitoso. Ahora puedes iniciar sesión.')
+        return super().form_valid(form)
+
+
+class UpdateProfileView(UpdateView):
+    model = User
+    form_class = UserProfileForm
+    template_name = 'core/profile.html'
+    success_url = reverse_lazy('core:index')
+
+    def get_object(self):
+        
+        return self.request.user
