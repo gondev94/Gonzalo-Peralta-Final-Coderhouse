4fb3409aa9112e58bc25017d55c3760162aeacb8
Gonzalo Peralta <gonnzaperalta94@gmail.com>
2024-12-26 11:49:57 -0300

    feat: proteccion de secret key

--- src/config/settings.py
+++ src/config/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os 
+import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+u&=v!ol($+s4d@_lf!2b0-n8#+l+kcg9*5c#+fb0avqe7h7@$'
+dotenv.load_dotenv(BASE_DIR / ".env")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
